f03fbccc3c77bc7a14a353142ce2aae8626e1bd8
seointernets <94245238+seointernets@users.noreply.github.com>
2021-11-14 01:48:35 +0700

    slide
    slide

---
+++ assets/inject.js
@@ -0,0 +1,81 @@
+let dbAds=[
+  {
+    "target-selector":[
+      ".container",
+      "#container",
+      ".content"
+    ],
+    "position":"out-top", //out-top, out-bottom, in-top, in-bottom
+    "data" :`
+    `,
+    "style":`
+      width: 90%;
+      margin: auto;
+      margin-bottom: 10px;
+      margin-top: 10px;
+    `
+  }
+];
+
+dbAds.forEach(function(a){
+  let createElDom=document.createElement("div");
+  createElDom.setAttribute("style",a["style"]);
+  createElDom.innerHTML=a["data"];
+  let dataScript=[];
+  createElDom.querySelectorAll("script").forEach(function(b){
+    let createElCostom=document.createElement("script");
+    createElCostom.innerHTML=b.innerHTML;
+    dataScript.push(createElCostom);
+    b.remove();
+  });
+  a["target-selector"].forEach(function(b){
+    let targetEl=document.querySelector(b);
+    if(targetEl){
+      if(a["position"]=="out-bottom"){
+        targetEl.parentNode.insertBefore(createElDom,targetEl.nextSibling);
+      }else if(a["position"]=="out-top"){
+        targetEl.parentNode.insertBefore(createElDom,targetEl.nextSibling);
+        createElDom.after(targetEl);
+      }else if(a["position"]=="in-top"){
+
+      }else if(a["position"]=="in-top"){
+        
+      };
+      dataScript.forEach(function(b){
+        createElDom.append(b); 
+      });
+    }else{
+      console.log("target "+a["target-selector"]+" tidak ditemukan"); 
+    };
+  });
+});
+
+
+let elImg=document.querySelectorAll("img");
+elImg.forEach(function(a){
+  a.setAttribute("style","max-width:100%");
+  let classImg=a.getAttribute("class");
+  let getSrcSet=a.getAttribute("srcset");
+  if(classImg==null==false){
+    a.classList.remove("lazyload");
+  };
+  if(getSrcSet==null==false){
+    getSrcSet=getSrcSet.split(",");
+    if(getSrcSet.length>1){
+      a.setAttribute("src",getSrcSet);
+    };
+  };
+});
+
+let dataLazy=document.querySelectorAll(".lazy-image.lazy-image-udf");
+dataLazy.forEach(function(a){
+  let dataHref=a.getAttribute("data-src");
+  if(dataHref){
+    let targetLazy=a.querySelector(".loadingPlaceholder");
+    let targetDiv=a.querySelector(".lazy-image__loadingPlaceholder")
+    if(targetLazy){
+      targetLazy.setAttribute("src",dataHref);
+      targetDiv.setAttribute("class","show")
+    };
+  };
+});
